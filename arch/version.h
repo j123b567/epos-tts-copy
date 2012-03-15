470ea6e4aa1915f1fbfaa1b477e6dbd2f905b123
stanek <stanek@905ca500-da46-4645-b043-b538ca7892eb>
2012-03-15 13:59:34 +0000

    priprava 2.5.36
    git-svn-id: https://svn.code.sf.net/p/epos/code/trunk@171 905ca500-da46-4645-b043-b538ca7892eb

--- arch/version.h
+++ arch/version.h
@@ -1,1 +1,1 @@
-#define VERSION "2.5.35"
+#define VERSION "2.5.36"
--- src/hashi.cc
+++ src/hashi.cc
@@ -18,7 +18,7 @@
 #include "epos.h"
 #include "hashtmpl.h"
 
-#include <netdb.h>
+// #include <netdb.h>
 
 /*
  *	This is a good place for hash table instantiation needed by both
@@ -33,12 +33,12 @@
 #ifdef HAVE_TEMPL_INST
 	template class hash_table<char, file>;
 	template class hash_table<char, epos_option>;
-	template class hash_table<char, hostent>;
+//	template class hash_table<char, hostent>;
 	template class hash_table<wchar, wchar>;
 #else	// e.g., a prehistoric WatcomC
 	extern hash_table<char, file> *_dummy_freadin_hash_tmpl_inst;	// remove this one asterisk if Watcom barfs, else remove this comment
 	extern hash_table<char, epos_option> *_dummy_option_hash_tmp_inst;
-	extern hash_table<char, hostent> *_dummy_hostent_hash_tmp_inst;
+//	extern hash_table<char, hostent> *_dummy_hostent_hash_tmp_inst;
 	extern hash_table<wchar, wchar> *_dummy_char_hash_tmpl_inst;
 #endif
 
