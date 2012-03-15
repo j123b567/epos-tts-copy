d2d64837b3b0dd594357968383c302d46ac0f60a
stanek <stanek@905ca500-da46-4645-b043-b538ca7892eb>
2012-03-15 09:21:07 +0000

    priprava 2.4.84
    git-svn-id: https://svn.code.sf.net/p/epos/code/branches/2.4.devel@92 905ca500-da46-4645-b043-b538ca7892eb

--- src/common.h
+++ src/common.h
@@ -27,7 +27,7 @@
 
 #define MAINTAINER  		"Jirka Hanika"
 #define MAIL        		"geo@cuni.cz"
-#define VERSION     		"2.4.83"
+#define VERSION     		"2.4.84"
 #define TTSCP_PORT		 8778
 
 #include "config.h"
